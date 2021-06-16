9674bc27c1b65e00daa4deae3ce220abec2ac75e
Auro <35087590+MrAuro@users.noreply.github.com>
2021-06-15 23:15:13 -0400

    Update index.ts

--- src/index.ts
+++ src/index.ts
@@ -153,7 +153,7 @@ async function main() {
         console.log(`${user} joined ${channel}`);
         // chatClient.say(channel, `MrDestructoid Joined channel!`);
 
-        // if (channel === '#auror6s') chatClient.say('auror6s', `PagMan BOT STARTED ${process.env.DEBUG === 'TRUE' ? 'IN DEBUG MODE' : ''}`);
+        if (channel === '#auror6s') chatClient.say('auror6s', `PagMan BOT STARTED ${process.env.DEBUG === 'TRUE' ? 'IN DEBUG MODE' : ''}`);
     });
 
     chatClient.onMessage(async (channel, user, message, msg) => {
