31361fdd01c8e813a68d174fab56b204eff01299
Auro <35087590+MrAuro@users.noreply.github.com>
2021-08-28 15:38:22 -0400

    Create filesayslow.ts

---
+++ src/commands/filesayslow.ts
@@ -0,0 +1,46 @@
+import dotenv from 'dotenv';
+import { Command, CommandReturnClass } from '../utils/commandClass';
+import { ChatClient } from 'twitch-chat-client';
+import { chatClient } from '../index';
+import axios from 'axios';
+
+dotenv.config();
+
+class testComand extends Command {
+    name = 'filesayslow';
+    description = 'Say a hastebin file with a 1.1 second delay';
+    usage = 'filesayslow <url> <optional msg prefix?>';
+    hidden = true;
+    permission = 1;
+    execute = async (user: string, channel: string, args: string[]): Promise<CommandReturnClass> => {
+        if (!args[0].match(/^https:\/\/(haste\.zneix\.eu\/|hastebin\.com\/|pastebin\.com\/)raw\/.+$/))
+            return {
+                success: false,
+                message: 'Missing RAW haste/pastebin link',
+                error: null,
+            };
+
+        axios.get(args[0]).then(async (response: any) => {
+            let msgs = response.data.split('\n');
+            if (args[1]) {
+                for (let msg of msgs) {
+                    await new Promise((resolve) => setTimeout(resolve, 1200));
+                    await chatClient.say(channel, `${args[1]} ${msg}`);
+                }
+            } else {
+                for (let msg of msgs) {
+                    await new Promise((resolve) => setTimeout(resolve, 1200));
+                    await chatClient.say(channel, msg);
+                }
+            }
+        });
+
+        return {
+            success: true,
+            message: null,
+            error: null,
+        };
+    };
+}
+
+export const cmd = new testComand();
