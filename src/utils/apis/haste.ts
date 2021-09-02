35586d8f69c450860378f9e62f94bc118c6ca1ef
Auro <35087590+MrAuro@users.noreply.github.com>
2021-09-01 23:19:43 -0400

    Update haste.ts

--- src/utils/apis/haste.ts
+++ src/utils/apis/haste.ts
@@ -2,5 +2,5 @@ import axios from 'axios';
 
 export async function upload(message: string): Promise<string> {
     let hasteRes = await axios.post('https://haste.zneix.eu/documents', message);
-    return `https://haste.zneix.eu/${hasteRes.data.key}`;
+    return `https://haste.zneix.eu/raw/${hasteRes.data.key}`;
 }
