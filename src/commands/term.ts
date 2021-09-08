c4a71e2a62b9d7d051105efee989dd9fe68fda25
matt <30363562+mmattbtw@users.noreply.github.com>
2021-09-07 20:41:56 -0500

    Added edit terms (#145)

--- src/commands/term.ts
+++ src/commands/term.ts
@@ -9,14 +9,14 @@ class testComand extends Command {
     name = 'term';
     description = 'Add or remove moderation terms';
     extendedDescription = 'Regex ignores case. Response can use "{newline}" to send more messages. Use / commands to time users out. Use {user} to get the user.';
-    usage = 'term <add|remove> <regex> <response>';
+    usage = 'term <add|edit|remove> <regex> <response>';
     permission = 4;
     userCooldown = 5;
     execute = async (user: string, channel: string, args: string[]): Promise<CommandReturnClass> => {
         if (!args[0])
             return {
                 success: false,
-                message: 'Missing subcommand (add,remove)',
+                message: 'Missing subcommand (add,edit,remove)',
                 error: null,
             };
 
@@ -70,6 +70,58 @@ class testComand extends Command {
                 error: null,
                 ignorebanphrase: true,
             };
+        } else if (args[0] === 'edit') {
+            if (!args[1])
+                return {
+                    success: false,
+                    message: 'Missing term regex',
+                    error: null,
+                };
+            if (!args[2])
+                return {
+                    success: false,
+                    message: 'Missing term response',
+                    error: null,
+                };
+
+            let termRegex = args[1];
+
+            // remove the first 2 elements of args
+            args.splice(0, 2);
+
+            let termResponse = args.join(' ');
+            if (termResponse.length > 400)
+                return {
+                    success: false,
+                    message: 'Term response must be less than 400 characters',
+                    error: null,
+                };
+
+            if (!termResponse)
+                return {
+                    success: false,
+                    message: 'Missing term response',
+                    error: null,
+                };
+
+            if (termRegex === '!{WEEB}') termRegex = WEEB_REGEX.toString().replace(/^\/|\/g$/g, '');
+
+            await Term.findOneAndRemove({ regex: termRegex })
+
+            let newTerm = new Term({
+                channel: channel.replace('#', ''),
+                regex: termRegex,
+                response: termResponse,
+            });
+
+            await newTerm.save();
+            redis.del(`tl:${channel}:term`);
+            return {
+                success: true,
+                message: `Term with a regex of "${termRegex.length > 400 ? '(REGEX TOO LONG)' : termRegex}" has been edited.`,
+                error: null,
+                ignorebanphrase: true,
+            };
         } else if (args[0] === 'remove') {
             if (!args[1]) {
                 return {
@@ -90,7 +142,7 @@ class testComand extends Command {
         } else {
             return {
                 success: false,
-                message: 'Invalid subcommand (add,remove)',
+                message: 'Invalid subcommand (add,edit,remove)',
                 error: null,
             };
         }
