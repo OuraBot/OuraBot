f8f63ac0117527dba95230f08ef1d914ca664d5e
matt <30363562+mmattbtw@users.noreply.github.com>
2021-09-12 12:34:38 -0500

    Added C3AGLE to the autoban follow bot list. (#156)

--- src/custommodules/se-follows.ts
+++ src/custommodules/se-follows.ts
@@ -11,7 +11,7 @@ import { obfuscateName } from '../utils/stringManipulation';
 class customModule extends CustomModule {
     name = 'se-follows';
     description = "Compares the user in StreamElement's follow chat message to a hash of known bots";
-    channels = ['#demonjoefrance', '#auror6s', '#jeffboys123', '#elpws', '#mmattbtw', '#docwaitingroom', '#liptongod'];
+    channels = ['#demonjoefrance', '#auror6s', '#jeffboys123', '#elpws', '#mmattbtw', '#docwaitingroom', '#liptongod', '#c3agle'];
     author = ['AuroR6S'];
     execute = async (channel: string, user: string, message: string, msg: TwitchPrivateMessage, chatClient: ChatClient, redis: Redis): Promise<void> => {
         if (user === 'auror6s' || user === 'streamelements' || user === 'fossabot') {
@@ -144,6 +144,24 @@ class customModule extends CustomModule {
                         }
                     }
                     break;
+                case '#c3agle': 
+                {
+                    if (message.match(/^([A-z0-9_]+) followed! peepoLove thank you/)) {
+                        const username = message.match(/^([A-z0-9_]+) followed! peepoLove thank you/)[1];
+                        console.log(username);
+                        if (KNOWN_BOT_LIST.has(username)) {
+                            chatClient.ban(channel, username, 'User is on known bot list (follow/hate/ip grabber bots)').catch((err) => {
+                                console.log(err); // Error handling here isnt too important
+                            });
+                        } else if (username.match(/.*h(o|0)ss0?/)) {
+                            chatClient.ban(channel, username, 'User is on known bot list (follow/hate/ip grabber bots)').catch((err) => {
+                                console.log(err); // Error handling here isnt too important
+                            });
+                            chatClient.say(config.owner, `${username} is NOT on the known bot list and has been banned in ${obfuscateName(channel)} - This was matched using the regex`);
+                        }
+                    }
+                }
+                break;
         }}
     };
 }
