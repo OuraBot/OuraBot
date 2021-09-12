5014e03b28a5f7916435c3182774d841bbfb32b7
Auro <35087590+MrAuro@users.noreply.github.com>
2021-09-11 23:32:09 -0400

    Renamed lastfm and added aliases

---
+++ src/commands/lastfmsong.ts
@@ -0,0 +1,48 @@
+import dotenv from 'dotenv';
+import { chatClient } from '..';
+import { Channel } from '../models/channel.model';
+import { Command, CommandReturnClass, ErrorEnum } from '../utils/commandClass';
+import { error } from '../utils/logger';
+import { getNowPlaying } from '../utils/apis/lastfm';
+import { getBestEmote } from '../utils/channelEmotes';
+import { obfuscateName } from '../utils/stringManipulation';
+dotenv.config();
+
+class lastfmCommand extends Command {
+    name = 'lastfmsong';
+    description = 'Gets the current playing song from the streamers Last.fm profile.';
+    usage = 'lastfmsong';
+    aliases = ['lastfm', 'np', 'nowplaying', 'currentsong'];
+    userCooldown = 10;
+    channelCooldown = 5;
+    execute = async (user: string, channel: string, args: string[]): Promise<CommandReturnClass> => {
+        let song = await getNowPlaying(channel);
+
+        if (!song.success) {
+            return {
+                success: false,
+                message: `${song.result}`,
+                error: null,
+            };
+        } else {
+            let preferredEmote = await getBestEmote(channel, ['forsenPls', 'Jammer', 'Jammies', 'forsenDiscoSnake', 'pepeJAM'], '🎶');
+            if (song.artist === 'Kanye West') preferredEmote = await getBestEmote(channel, ['TriKool', 'TriDance'], '🎶');
+
+            if (song.result) {
+                return {
+                    success: true,
+                    message: `${obfuscateName(song.LastfmUsername)}'s most recent track is: ${song.result} - ${song.artist} ${preferredEmote.bestAvailableEmote}`,
+                    error: null,
+                };
+            } else {
+                return {
+                    success: true,
+                    message: `No recent tracks found for ${obfuscateName(channel.replace('#', ''))}`,
+                    error: null,
+                };
+            }
+        }
+    };
+}
+
+export const cmd = new lastfmCommand();
