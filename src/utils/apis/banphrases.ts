58ce24bb6358627d04d6445636d0541971a20424
matt <30363562+mmattbtw@users.noreply.github.com>
2021-09-19 22:38:42 -0500

    Strip the route for pajbot banphrase API (#162)

--- src/utils/apis/banphrases.ts
+++ src/utils/apis/banphrases.ts
@@ -8,24 +8,22 @@ export class PajbotApi {
 }
 
 export async function checkPajbotBanphrase(message: string, channel: string): Promise<Boolean> {
-    const banphraseURL =  JSON.parse(await redis.get(`ob:pajbotbanphrase`));
+    const banphraseURL = JSON.parse(await redis.get(`ob:pajbotbanphrase`));
     if (!banphraseURL.map((e: PajbotApi) => e.channel).includes(channel.replace('#', ''))) return false;
     let obj = banphraseURL.filter((e: PajbotApi) => e.channel === channel.replace('#', ''));
 
     let resp;
     try {
-        resp = await axios.post(obj[0].url, {
-            message: message
+        resp = await axios.post(`${obj[0].url.replace('/api/v1/banphrases/test', '')}/api/v1/banphrases/test`, {
+            message: message,
         });
     } catch (err) {
-        return false;   
+        return false;
     }
-    
 
     if (resp.data.banned === true) {
         return true;
     } else {
         return false;
     }
-        
 }
