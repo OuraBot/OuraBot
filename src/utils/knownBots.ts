f45df3c135ad5808a482b5cddfa368ea433b6ab4
Auro <35087590+MrAuro@users.noreply.github.com>
2021-09-24 00:33:37 -0400

    Update knownBots.ts

--- src/utils/knownBots.ts
+++ src/utils/knownBots.ts
@@ -3,7 +3,7 @@ import axios from 'axios';
 export let KNOWN_BOT_LIST: Set<string>;
 
 export async function fetchBots(): Promise<Set<string>> {
-    const bots = (await axios.get(`https://api.github.com/gists/68ec520b109c7f93d55c6cab4ffc7659`)).data.files['bots.txt'].content;
+    const bots = (await axios.get(`https://raw.githubusercontent.com/LinoYeen/Namelists/main/namelist.txt`)).data;
     KNOWN_BOT_LIST = new Set(bots.toString().split('\n'));
     return KNOWN_BOT_LIST;
 }
