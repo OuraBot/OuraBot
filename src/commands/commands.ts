5de092f803617d6ed18efd898116ea921c070c5b
Auro <35087590+MrAuro@users.noreply.github.com>
2021-10-11 21:04:02 -0400

    fix commands command not posting custom commands

--- src/commands/commands.ts
+++ src/commands/commands.ts
@@ -4,7 +4,7 @@ import { createNewSuggestion } from '../models/suggestion.model';
 import { Command, CommandReturnClass, getCommands, PermissionEnum } from '../utils/commandClass';
 import { getChannels } from '../utils/fetchChannels';
 import { prettyTime } from '../utils/auroMs';
-import { CustomCommand } from '../models/command.model';
+import { CustomCommand, ICustomCommand } from '../models/command.model';
 import axios from 'axios';
 import { upload } from '../utils/apis/haste';
 dotenv.config();
@@ -17,16 +17,13 @@ class suggestCommand extends Command {
     channelCooldown = 60;
     execute = async (user: string, channel: string, args: string[]): Promise<CommandReturnClass> => {
         let data = `Commands for ${channel} | ${process.env.CLIENT_USERNAME} | ${new Date()}\n\n`;
-        let prefixed = false;
-        CustomCommand.find().then((commands) => {
-            for (let command of commands.filter((c) => c.channel === channel.replace('#', ''))) {
-                if (!prefixed) {
-                    data += `Custom Commands:\n\n`;
-                    prefixed = true;
-                }
-                data += `Command: ${command.command}\nResponse: ${command.response}\nUser Cooldown: ${command.userCooldown}\nChannel Cooldown: ${command.channelCooldown}\n\n`;
-            }
-        });
+
+        const customCommands: ICustomCommand[] = await CustomCommand.find({ channel: channel.replace('#', '') });
+
+        if (customCommands.length > 0) data += `Channel Commands:\n\n`;
+        for (let command of customCommands) {
+            data += `Command: ${command.command}\nResponse: ${command.response}\nUser Cooldown: ${command.userCooldown}\nChannel Cooldown: ${command.channelCooldown}\n\n`;
+        }
 
         data += `Global Commands:\n\n`;
 
