574e87636711f92d1888651b30ef4e969cec04c0
Auro <35087590+MrAuro@users.noreply.github.com>
2021-11-10 00:37:32 -0500

    Refined requestjoin discord notification

--- src/custommodules/requestjoin.ts
+++ src/custommodules/requestjoin.ts
@@ -3,6 +3,7 @@ import { TwitchPrivateMessage } from '@twurple/chat/lib/commands/TwitchPrivateMe
 import axios from 'axios';
 import { Redis } from 'ioredis';
 import { CustomModule } from '../types/custommodule';
+import { getAllEmotes, getBttvChannelEmotes, getFfzChannelEmotes, getStvChannelEmotes } from '../utils/channelEmotes';
 
 class customModule extends CustomModule {
     name = 'requestjoin';
@@ -10,12 +11,44 @@ class customModule extends CustomModule {
     channels = ['#auror6s', '#oura_bot'];
     author = ['AuroR6S'];
     execute = async (channel: string, user: string, message: string, msg: TwitchPrivateMessage, chatClient: ChatClient, redis: Redis): Promise<void> => {
-        if (message.startsWith('!requestjoin')) {
+        if (message == '!requestjoin') {
             chatClient.say(channel, `@${user}, The bot has been requested to join your channel! Please wait for one of the admins to add it to your channel`);
+
+            let recentMessages = null;
+            try {
+                recentMessages = await axios.get(`https://recent-messages.robotty.de/api/v2/recent-messages/${user}`);
+            } catch (e) {
+                recentMessages.data.messages = [];
+            }
+
             axios.post(process.env.DISCORD_WEBHOOK, {
                 username: 'Oura_Bot Request',
-                content: `@everyone ${user} has requested the bot to join their channel! (${msg.userInfo.userId})`,
+                content: `@everyone **${user}** has requested the bot to join their channel! (${msg.userInfo.userId})\nFFZ Emotes: **${
+                    (await getFfzChannelEmotes(user)).split(' ').length
+                }**\nBTTV Emotes: **${(await getBttvChannelEmotes(user)).split(' ').length}**\n7TV Emotes: **${(await getStvChannelEmotes(user)).length}**\nRecent Messages: **${
+                    recentMessages.data.messages.length
+                }**`,
             });
+        } else if (message.toLowerCase().startsWith(`!requestjoin ${user.toLowerCase()}`)) {
+            chatClient.say(channel, `@${user}, The bot has been requested to join your channel! Please wait for one of the admins to add it to your channel`);
+
+            let recentMessages = null;
+            try {
+                recentMessages = await axios.get(`https://recent-messages.robotty.de/api/v2/recent-messages/${user}`);
+            } catch (e) {
+                recentMessages.data.messages = [];
+            }
+
+            axios.post(process.env.DISCORD_WEBHOOK, {
+                username: 'Oura_Bot Request',
+                content: `@everyone **${user}** has requested the bot to join their channel! (${msg.userInfo.userId})\nFFZ Emotes: **${
+                    (await getFfzChannelEmotes(user)).split(' ').length
+                }**\nBTTV Emotes: **${(await getBttvChannelEmotes(user)).split(' ').length}**\n7TV Emotes: **${(await getStvChannelEmotes(user)).length}**\nRecent Messages: **${
+                    recentMessages.data.messages.length
+                }**`,
+            });
+        } else if (message.startsWith('!requestjoin')) {
+            chatClient.say(channel, `@${user}, Only the broadcaster can request me to join their channel. Please ask them to use the "!requestjoin" command here.`);
         }
     };
 }
