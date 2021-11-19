dc5a2ca906312e957cae342aca7416da827bf3cb
Auro <35087590+MrAuro@users.noreply.github.com>
2021-11-18 20:06:19 -0500

    Refactored stats command to focus on speed, violence, and momentum
    forsenCD

--- src/commands/stats.ts
+++ src/commands/stats.ts
@@ -1,118 +1,60 @@
 import dotenv from 'dotenv';
-import { commands } from '../index';
+import { Channel } from '../models/channel.model';
 import { IUsage, Usage } from '../models/usage.model';
 import { Command, CommandReturnClass } from '../utils/commandClass';
-import { obfuscateName } from '../utils/stringManipulation';
 
 dotenv.config();
 
 class testComand extends Command {
     name = 'stats';
-    description = 'Gets the stats of a command, user, channel, or globally (since Oct 7 2021)';
-    usage = 'stats (command|@user|#channel|<leave empty for global stats>) ';
-    userCooldown = 5;
-    channelCooldown = 1;
+    description = 'Get stats about the bot, including the most used command';
+    usage = 'stats';
+    userCooldown = 10;
+    channelCooldown = 5;
     execute = async (user: string, channel: string, args: string[]): Promise<CommandReturnClass> => {
         if (!args[0]) {
-            let commandsExecuted: number, successCount: number, mostCommonCommand: string, mostCommonCommandCount: number;
-            const usages = await Usage.find({});
-            if (usages.length === 0)
-                return {
-                    success: false,
-                    message: `No commands have been used`,
-                    error: null,
-                };
+            const usages = await Usage.aggregate([
+                {
+                    $group: {
+                        _id: {
+                            command: '$command',
+                        },
+                        count: {
+                            $sum: 1,
+                        },
+                    },
+                },
+                {
+                    $sort: {
+                        '_id.command': 1,
+                        count: -1,
+                    },
+                },
+                {
+                    $group: {
+                        _id: {
+                            command: '$_id.command',
+                        },
+                        count: {
+                            $first: '$count',
+                        },
+                    },
+                },
+            ]);
 
-            commandsExecuted = usages.length;
-            mostCommonCommand = getMostCommonCommand(usages);
-            mostCommonCommandCount = usages.filter((usage: IUsage) => usage.command === mostCommonCommand).length;
-
-            successCount = usages.filter((usage: IUsage) => usage.success).length;
-
-            return {
-                success: true,
-                message: `${commandsExecuted} commands have been used. The most used command is ${mostCommonCommand} at ${mostCommonCommandCount} uses. ${Math.round(
-                    (successCount / commandsExecuted) * 100
-                )}% of the commands have been successful.`,
-                error: null,
-            };
-        } else if (args[0].startsWith('@')) {
-            let commandsExecuted: number, mostCommonCommand: string, mostCommonCommandCount: number, successCount: number;
-            const usages = await Usage.find({ user: args[0].slice(1).toLowerCase() });
-            if (usages.length === 0)
-                return {
-                    success: false,
-                    message: `${args[0]} has not used any commands`,
-                    error: null,
-                };
-
-            commandsExecuted = usages.length;
-            mostCommonCommand = getMostCommonCommand(usages);
-            mostCommonCommandCount = usages.filter((usage: IUsage) => usage.command === mostCommonCommand).length;
-
-            successCount = usages.filter((usage: IUsage) => usage.success).length;
+            let mostUsedCommand = usages.sort((a, b) => b.count - a.count)[0];
+            let totalCount = usages.reduce((a, b) => a + b.count, 0);
+            let channelCount = await Channel.countDocuments({});
 
             return {
                 success: true,
-                message: `${args[0].slice(1).toLowerCase() === user ? 'You have' : `User ${obfuscateName(args[0]).slice(1).toLowerCase()} has`} used ${commandsExecuted} commands. ${
-                    args[0].slice(1).toLowerCase() === user ? 'Your' : 'Their'
-                } most used command is ${mostCommonCommand} at ${mostCommonCommandCount} uses. ${Math.round((successCount / commandsExecuted) * 100)}% of ${
-                    args[0].slice(1).toLowerCase() === user ? 'your' : 'their'
-                } commands have been successful.`,
-                error: null,
-            };
-        } else if (args[0].startsWith('#')) {
-            let commandsExecuted: number, mostCommonCommand: string, mostCommonCommandCount: number, successCount: number;
-            const usages = await Usage.find({ channel: args[0].toLowerCase() });
-            if (usages.length === 0)
-                return {
-                    success: false,
-                    message: `Channel ${args[0]} has not used any commands`,
-                    error: null,
-                };
-
-            commandsExecuted = usages.length;
-            mostCommonCommand = getMostCommonCommand(usages);
-            mostCommonCommandCount = usages.filter((usage: IUsage) => usage.command === mostCommonCommand).length;
-
-            successCount = usages.filter((usage: IUsage) => usage.success).length;
-
-            return {
-                success: true,
-                message: `Channel ${obfuscateName(
-                    args[0]
-                ).toLowerCase()} has used ${commandsExecuted} commands. The most used command there is ${mostCommonCommand} at ${mostCommonCommandCount} uses. ${Math.round(
-                    (successCount / commandsExecuted) * 100
-                )}% of the commands executed there have been successful.`,
-                error: null,
-            };
-        } else if ((await commands).has(args[0])) {
-            let commandsExecuted: number, successCount: number, mostCommonUser: string, mostCommonUserCount: number;
-            const usages = await Usage.find({ command: args[0] });
-            if (usages.length === 0)
-                return {
-                    success: false,
-                    message: `Command ${args[0]} has not been used`,
-                    error: null,
-                };
-
-            commandsExecuted = usages.length;
-            mostCommonUser = getMostCommonUser(usages);
-            mostCommonUserCount = usages.filter((usage: IUsage) => usage.user === mostCommonUser).length;
-
-            successCount = usages.filter((usage: IUsage) => usage.success).length;
-
-            return {
-                success: true,
-                message: `Command ${args[0]} has been used ${commandsExecuted} times. The most used user of this command is @${obfuscateName(
-                    mostCommonUser
-                )} at ${mostCommonUserCount} uses. ${Math.round((successCount / commandsExecuted) * 100)}% of the executions of this command have been successful.`,
+                message: `I am currently in ${channelCount} channels. ${totalCount} commands have been used. The most used command is ${mostUsedCommand._id.command} at ${mostUsedCommand.count} uses.`,
                 error: null,
             };
         } else {
             return {
-                success: false,
-                message: `Command ${args[0]} does not exist`,
+                success: true,
+                message: 'The ability to view the stats for a specific command/user/channel has been removed (since Nov 18 2021)',
                 error: null,
             };
         }
