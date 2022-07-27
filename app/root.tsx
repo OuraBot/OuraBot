c4029210a4cb90f481f7bae1205555780be84998
Auro <35087590+MrAuro@users.noreply.github.com>
2022-07-27 17:48:10 -0400

    Improved Error & Catch Boundaries

--- app/root.tsx
+++ app/root.tsx
@@ -1,4 +1,4 @@
-import { Button, ColorScheme, createEmotionCache, Title } from '@mantine/core';
+import { Button, ColorScheme, Container, createEmotionCache, Paper, Title, Text, ScrollArea } from '@mantine/core';
 import { MantineProvider, ColorSchemeProvider } from '@mantine/core';
 import { useColorScheme } from '@mantine/hooks';
 import { ModalsProvider } from '@mantine/modals';
@@ -6,11 +6,12 @@ import { useState } from 'react';
 import { Link } from 'react-router-dom';
 import { LinksFunction, LoaderFunction, MetaFunction } from '@remix-run/node';
 
-import { Links, LiveReload, Meta, Outlet, Scripts, ScrollRestoration, useLoaderData } from '@remix-run/react';
+import { Links, LiveReload, Meta, Outlet, Scripts, ScrollRestoration, useCatch, useLoaderData } from '@remix-run/react';
 
 import dbConnect from './services/mongo.server';
 import { NotificationsProvider } from '@mantine/notifications';
 import { StylesPlaceholder } from '@mantine/remix';
+import { StatusCodes } from './services/redis.server';
 
 export const meta: MetaFunction = () => ({
 	charset: 'utf-8',
@@ -24,9 +25,7 @@ export const loader: LoaderFunction = async () => {
 	return true;
 };
 
-export default function App() {
-	const connected = useLoaderData();
-
+export function Mantine({ children }: { children: React.ReactNode }) {
 	return (
 		<MantineProvider
 			theme={{
@@ -41,49 +40,131 @@ export default function App() {
 						<head>
 							<Meta />
 							<Links />
+							<LiveReload />
 							<StylesPlaceholder />
 						</head>
 						<body>
-							<Outlet />
-							<ScrollRestoration />
+							{children}
 							<Scripts />
-							<LiveReload />
 						</body>
 					</html>
 				</ModalsProvider>
 			</NotificationsProvider>
 		</MantineProvider>
+	);
+}
+
+export default function App() {
+	const connected = useLoaderData();
+
+	return (
+		<Mantine>
+			<Outlet />
+			<ScrollRestoration />
+		</Mantine>
 	);
 }
 
 export function ErrorBoundary({ error }: { error: Error }) {
 	return (
-		<MantineProvider
-			theme={{
-				colorScheme: 'dark',
-			}}
-			withGlobalStyles
-			withNormalizeCSS
-		>
-			<NotificationsProvider>
-				<ModalsProvider>
-					<html lang="en">
-						<head>
-							<Meta />
-							<Links />
-							<StylesPlaceholder />
-						</head>
-						<body>
-							<Title>App Error</Title>
-							<pre>{error.message}</pre>
-							<pre>{error.stack}</pre>
-							<Link to="/">
-								<Button>Go Home</Button>
-							</Link>
-						</body>
-					</html>
-				</ModalsProvider>
-			</NotificationsProvider>
-		</MantineProvider>
+		<Mantine>
+			<Container size="lg">
+				<Paper withBorder shadow="md" p="lg" mt="lg" radius="md">
+					<Title>Error</Title>
+					<Text>
+						An unknown error has occured. Click{' '}
+						<Text variant="link" component="a" href="/">
+							here
+						</Text>{' '}
+						to return to the homepage.
+					</Text>
+					<ScrollArea>
+						<pre>{error.stack}</pre>
+					</ScrollArea>
+				</Paper>
+			</Container>
+		</Mantine>
 	);
 }
+
+export function CatchBoundary() {
+	const caught = useCatch();
+	return (
+		<Mantine>
+			<Container size="lg">
+				<Paper withBorder shadow="md" p="lg" mt="lg" radius="md">
+					<Title>
+						{caught.status} {StatusCodes[caught.status as keyof typeof StatusCodes]}
+					</Title>
+					<Text>{caught.status === 404 ? 'Page not found' : (caught.data as string).replace(/"/g, '')}</Text>
+					<Text>
+						Click{' '}
+						<Text variant="link" component="a" href="/">
+							here
+						</Text>{' '}
+						to return to the homepage.
+					</Text>
+				</Paper>
+			</Container>
+		</Mantine>
+	);
+}
+
+const StatusCodes = {
+	100: 'Continue',
+	101: 'Switching Protocols',
+	102: 'Processing',
+	200: 'OK',
+	201: 'Created',
+	202: 'Accepted',
+	203: 'Non Authoritative Information',
+	204: 'No Content',
+	205: 'Reset Content',
+	206: 'Partial Content',
+	207: 'Multi-Status',
+	300: 'Multiple Choices',
+	301: 'Moved Permanently',
+	302: 'Moved Temporarily',
+	303: 'See Other',
+	304: 'Not Modified',
+	305: 'Use Proxy',
+	307: 'Temporary Redirect',
+	308: 'Permanent Redirect',
+	400: 'Bad Request',
+	401: 'Unauthorized',
+	402: 'Payment Required',
+	403: 'Forbidden',
+	404: 'Not Found',
+	405: 'Method Not Allowed',
+	406: 'Not Acceptable',
+	407: 'Proxy Authentication Required',
+	408: 'Request Timeout',
+	409: 'Conflict',
+	410: 'Gone',
+	411: 'Length Required',
+	412: 'Precondition Failed',
+	413: 'Request Entity Too Large',
+	414: 'Request-URI Too Long',
+	415: 'Unsupported Media Type',
+	416: 'Requested Range Not Satisfiable',
+	417: 'Expectation Failed',
+	418: "I'm a teapot",
+	419: 'Insufficient Space on Resource',
+	420: 'Method Failure',
+	421: 'Misdirected Request',
+	422: 'Unprocessable Entity',
+	423: 'Locked',
+	424: 'Failed Dependency',
+	428: 'Precondition Required',
+	429: 'Too Many Requests',
+	431: 'Request Header Fields Too Large',
+	451: 'Unavailable For Legal Reasons',
+	500: 'Internal Server Error',
+	501: 'Not Implemented',
+	502: 'Bad Gateway',
+	503: 'Service Unavailable',
+	504: 'Gateway Timeout',
+	505: 'HTTP Version Not Supported',
+	507: 'Insufficient Storage',
+	511: 'Network Authentication Required',
+};
