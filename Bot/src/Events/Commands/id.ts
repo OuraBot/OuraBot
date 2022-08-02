52e1c1ef2af266c47f98cd1bb18fba1749ad8165
Auro <35087590+MrAuro@users.noreply.github.com>
2022-08-02 15:32:26 -0400

    Added id command

---
+++ Bot/src/Events/Commands/id.ts
@@ -0,0 +1,33 @@
+import OuraBot from '../../Client';
+import { CategoryEnum, Channel, Command, CommandReturn } from '../../Typings/Twitch';
+import { TwitchPrivateMessage } from '@twurple/chat/lib/commands/TwitchPrivateMessage';
+
+export const cmd = new (class command implements Command {
+	name = 'id';
+	description = 'Get the Twitch ID of a user or yourself';
+	usage = 'id <user?>';
+	userCooldown = 1;
+	channelCooldown = 1;
+	modifiablePermissions = true;
+	category = CategoryEnum.Utility;
+	execute = async (ob: OuraBot, user: string, Channel: Channel, args: string[], message: string, msg: TwitchPrivateMessage, alias: string): Promise<CommandReturn> => {
+		if (args[0]) {
+			const userResp = await ob.utils.resolveUserByUsername(ob.utils.sanitizeName(args[0]));
+			if (!userResp)
+				return {
+					success: false,
+					message: 'User not found',
+				};
+
+			return {
+				success: true,
+				message: `${await ob.utils.smartObfuscate(Channel, userResp.displayName, user)} => ${userResp.id}`,
+			};
+		} else {
+			return {
+				success: true,
+				message: `${user} => ${msg.userInfo.userId}`,
+			};
+		}
+	};
+})();
