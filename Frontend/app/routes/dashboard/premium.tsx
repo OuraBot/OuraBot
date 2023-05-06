d94c7163ca6d3d904e5384da1a6a81a4b867b440
Auro <35087590+MrAuro@users.noreply.github.com>
2023-05-06 14:06:15 -0400

    Comment out premium page

--- Frontend/app/routes/dashboard/premium.tsx
+++ Frontend/app/routes/dashboard/premium.tsx
@@ -121,273 +121,287 @@ export const action: ActionFunction = async ({ request, params }) => {
 };
 
 export default function Premium() {
-	const [recepient, setRecepient] = useState('');
-	const [checkout, setCheckout] = useState(false);
-	const [months, setMonths] = useState(3);
-	const [realMonths, setRealMonths] = useState(3);
-	const { cid, channel, subscribed, expiresAt } = useLoaderData();
-	const actionData = useActionData();
-	const [error, setError] = useState('');
-	const [gifting, setGifting] = useState(subscribed);
-	const [queriedLogin, setQueriedLogin] = useState('');
-	const theme = useMantineTheme();
-	const modals = useModals();
-	const [shownModal, setShownModal] = useState(false);
-
-	const handleCheckout = async () => {
-		if (gifting) {
-			if (recepient.toLowerCase() === channel.login.toLowerCase()) return setError('You cannot gift to yourself');
-			if (queriedLogin === recepient) return setError('Please enter a different recepient');
-			setQueriedLogin(recepient);
-
-			const user: UserResponse | null = await fetch('/api/v3/user/' + recepient)
-				.then((res) => res.json())
-				.catch(() => setError('User not found'));
-			// setCheckout(true);
-
-			console.log(user);
-
-			if (!user) return setError('User not found');
-
-			if (user.premium.active == false) {
-				setCheckout(true);
-				setRealMonths(months);
-			} else {
-				return setError('User already has premium');
-			}
-		} else {
-			const user: UserResponse | null = await fetch('/api/v3/user/' + channel.login)
-				.then((res) => res.json())
-				.catch(() => setError('User not found'));
-
-			if (!user) return setError('User not found');
-			if (user.premium.active == true) return setError('You already have premium');
-			setCheckout(true);
-			setRealMonths(months);
-		}
-	};
-
-	if (actionData && !actionData.error && !shownModal) {
-		setShownModal(true);
-		modals.openModal({
-			size: 'lg',
-			overlayBlur: 0.75,
-			overlayColor: theme.colorScheme === 'dark' ? theme.colors.dark[9] : theme.colors.gray[2],
-			overlayOpacity: 0.55,
-			transition: 'fade',
-			transitionDuration: 300,
-			transitionTimingFunction: 'ease',
-			title: (
-				<>
-					<Title order={3}>Thank you for your purchase!</Title>
-				</>
-			),
-			children: (
-				<>
-					{actionData?.recepient === channel.login ? (
-						<>
-							<Text>
-								You have subscribed to premium for {actionData?.months} {actionData?.months === 1 ? 'month' : 'months'}. It will expire on{' '}
-								{new Date(actionData?.expiresAt)?.toUTCString()}.
-							</Text>
-							<Text>It really helps out a lot, so thank you for your support! {'<3'}</Text>
-							<Text>If you need any help, please contact support</Text>
-						</>
-					) : (
-						<>
-							<Text>
-								You have just gifted premium to {actionData?.recepient} for {actionData?.months} {actionData?.months === 1 ? 'month' : 'months'}. It will
-								expire on {new Date(actionData?.expiresAt)?.toUTCString()}.
-							</Text>
-							<Text>It really helps out a lot, so thank you for your support! {'<3'}</Text>
-							<Text>If you need any help, please contact support</Text>
-						</>
-					)}
-				</>
-			),
-		});
-	}
-
 	return (
 		<>
-			{subscribed ? (
-				<>
-					<Title order={3}>You already have premium, thanks for your support!</Title>
-					<Text>You can gift premium to other people below</Text>
-					<Text>
-						Your premium will expire on <strong>{new Date(expiresAt)?.toUTCString()}</strong>
-					</Text>
-					<Divider variant="solid" my="md" />
-				</>
-			) : null}
-			<Collapse in={!checkout}>
-				<Text>
-					Hosting and maintaining this bot is unfortunately not free. You can support the developer by purchasing a <strong>non-recurring</strong> payment.
+			<Title order={3}>Premium will be available in early Q4 of 2023.</Title>
+			<Text>
+				If you would like to support OuraBot in the meantime, please consider subscribing to the developer's{' '}
+				<Text component="a" href="https://twitch.tv/auror6s/subscribe" variant="link" target="_blank">
+					Twitch channel.
 				</Text>
-				<Text>
-					If you have any questions, please email <strong>contact@ourabot.com</strong>. All payments are non-refundable.
-				</Text>
-				<Box>
-					<Paper
-						m="sm"
-						style={{
-							backgroundColor: 'transparent',
-						}}
-					>
-						{/* 
-						<Card.Section>
-							<Text mb="xs" weight={500}>
-								Premium Features:
-							</Text>
-							<Grid grow>
-								<Grid.Col md={6} lg={3}>
-									<Card>
-										<Title order={4}>Feature Title</Title>
-										<Text>Feature description will go here, describing what the feature does.</Text>
-									</Card>
-								</Grid.Col>
-								<Grid.Col md={6} lg={3}>
-									<Card>
-										<Title order={4}>Feature Title</Title>
-										<Text>Feature description will go here, describing what the feature does.</Text>
-									</Card>
-								</Grid.Col>
-								<Grid.Col md={6} lg={3}>
-									<Card>
-										<Title order={4}>Feature Title</Title>
-										<Text>Feature description will go here, describing what the feature does.</Text>
-									</Card>
-								</Grid.Col>
-								<Grid.Col md={6} lg={3}>
-									<Card>
-										<Title order={4}>Feature Title</Title>
-										<Text>Feature description will go here, describing what the feature does.</Text>
-									</Card>
-								</Grid.Col>
-							</Grid>
-						</Card.Section>
-							*/}
-					</Paper>
-				</Box>
-
-				<Divider variant="solid" my="md" />
-				<Slider
-					my="lg"
-					value={months}
-					onChange={setMonths}
-					min={1}
-					max={12}
-					label={(value) => `${value} month${value == 1 ? '' : 's'}`}
-					mx="md"
-					marks={[
-						{ value: 1 },
-						{ value: 2 },
-						{ value: 3 },
-						{ value: 4 },
-						{ value: 5 },
-						{ value: 6 },
-						{ value: 7 },
-						{ value: 8 },
-						{ value: 9 },
-						{ value: 10 },
-						{ value: 11 },
-						{ value: 12 },
-					]}
-				/>
-				<Title order={2}>
-					{`$${pricePoints[months]} USD`}{' '}
-					{months > 4 && (
-						<>
-							<Badge size="md" variant="filled">
-								Save {Math.round((100 * (4 * months - pricePoints[months])) / (4 * months))}%
-							</Badge>
-						</>
-					)}
-				</Title>
-				<Text size="lg" mb="md">
-					{months} month{months == 1 ? '' : 's'}
-				</Text>
-
-				<Checkbox
-					label="Gift to another user"
-					checked={gifting}
-					onChange={(event) => {
-						if (subscribed && !event.target.checked) return;
-						setGifting(event.target.checked);
-						setRecepient('');
-					}}
-				/>
-				<Collapse in={gifting}>
-					<TextInput
-						label="Gift to"
-						error={error}
-						autoFocus
-						placeholder="Username"
-						value={recepient}
-						onChange={(event) => {
-							setRecepient(event.target.value);
-							setError('');
-						}}
-					/>
-				</Collapse>
-				<Button
-					my="md"
-					fullWidth
-					onClick={() => {
-						handleCheckout();
-					}}
-				>
-					Checkout
-				</Button>
-			</Collapse>
-			{checkout && (
-				<>
-					<UnstyledButton
-						onClick={() => {
-							setCheckout(false);
-						}}
-					>
-						<ArrowBackUp />
-					</UnstyledButton>
-					<Center>
-						<Stack>
-							<Text size="lg" align="center">
-								<strong>
-									${pricePoints[realMonths]} · {realMonths} month{realMonths == 1 ? '' : 's'}
-								</strong>
-							</Text>
-							{console.log(channel)}
-							<Text align="center">
-								<Text
-									variant="link"
-									component="a"
-									href={`https://twitch.tv/${gifting && recepient.length > 0 ? recepient : channel.login}`}
-									target="_blank"
-								>
-									https://twitch.tv/{gifting && recepient.length > 0 ? recepient : channel.login}
-								</Text>
-							</Text>
-							<PayPalScriptProvider
-								options={{
-									'client-id': cid,
-									components: 'buttons',
-									currency: 'USD',
-								}}
-							>
-								<ButtonWrapper
-									amount={pricePoints[realMonths]}
-									currency={currency}
-									showSpinner={true}
-									recepient={gifting && recepient.length > 0 ? recepient : channel.login}
-									months={realMonths}
-								/>
-							</PayPalScriptProvider>
-						</Stack>
-					</Center>
-				</>
-			)}
+			</Text>
+			<Text>In then future, you will be able to purchase Premium through PayPal on this page and recieve benefits.</Text>
 		</>
 	);
+
+	// UNCOMMENT AFTER 2023 Q3
+	// const [recepient, setRecepient] = useState('');
+	// const [checkout, setCheckout] = useState(false);
+	// const [months, setMonths] = useState(3);
+	// const [realMonths, setRealMonths] = useState(3);
+	// const { cid, channel, subscribed, expiresAt } = useLoaderData();
+	// const actionData = useActionData();
+	// const [error, setError] = useState('');
+	// const [gifting, setGifting] = useState(subscribed);
+	// const [queriedLogin, setQueriedLogin] = useState('');
+	// const theme = useMantineTheme();
+	// const modals = useModals();
+	// const [shownModal, setShownModal] = useState(false);
+
+	// const handleCheckout = async () => {
+	// 	if (gifting) {
+	// 		if (recepient.toLowerCase() === channel.login.toLowerCase()) return setError('You cannot gift to yourself');
+	// 		if (queriedLogin === recepient) return setError('Please enter a different recepient');
+	// 		setQueriedLogin(recepient);
+
+	// 		const user: UserResponse | null = await fetch('/api/v3/user/' + recepient)
+	// 			.then((res) => res.json())
+	// 			.catch(() => setError('User not found'));
+	// 		// setCheckout(true);
+
+	// 		console.log(user);
+
+	// 		if (!user) return setError('User not found');
+
+	// 		if (user.premium.active == false) {
+	// 			setCheckout(true);
+	// 			setRealMonths(months);
+	// 		} else {
+	// 			return setError('User already has premium');
+	// 		}
+	// 	} else {
+	// 		const user: UserResponse | null = await fetch('/api/v3/user/' + channel.login)
+	// 			.then((res) => res.json())
+	// 			.catch(() => setError('User not found'));
+
+	// 		if (!user) return setError('User not found');
+	// 		if (user.premium.active == true) return setError('You already have premium');
+	// 		setCheckout(true);
+	// 		setRealMonths(months);
+	// 	}
+	// };
+
+	// if (actionData && !actionData.error && !shownModal) {
+	// 	setShownModal(true);
+	// 	modals.openModal({
+	// 		size: 'lg',
+	// 		overlayBlur: 0.75,
+	// 		overlayColor: theme.colorScheme === 'dark' ? theme.colors.dark[9] : theme.colors.gray[2],
+	// 		overlayOpacity: 0.55,
+	// 		transition: 'fade',
+	// 		transitionDuration: 300,
+	// 		transitionTimingFunction: 'ease',
+	// 		title: (
+	// 			<>
+	// 				<Title order={3}>Thank you for your purchase!</Title>
+	// 			</>
+	// 		),
+	// 		children: (
+	// 			<>
+	// 				{actionData?.recepient === channel.login ? (
+	// 					<>
+	// 						<Text>
+	// 							You have subscribed to premium for {actionData?.months} {actionData?.months === 1 ? 'month' : 'months'}. It will expire on{' '}
+	// 							{new Date(actionData?.expiresAt)?.toUTCString()}.
+	// 						</Text>
+	// 						<Text>It really helps out a lot, so thank you for your support! {'<3'}</Text>
+	// 						<Text>If you need any help, please contact support</Text>
+	// 					</>
+	// 				) : (
+	// 					<>
+	// 						<Text>
+	// 							You have just gifted premium to {actionData?.recepient} for {actionData?.months} {actionData?.months === 1 ? 'month' : 'months'}. It will
+	// 							expire on {new Date(actionData?.expiresAt)?.toUTCString()}.
+	// 						</Text>
+	// 						<Text>It really helps out a lot, so thank you for your support! {'<3'}</Text>
+	// 						<Text>If you need any help, please contact support</Text>
+	// 					</>
+	// 				)}
+	// 			</>
+	// 		),
+	// 	});
+	// }
+
+	// return (
+	// 	<>
+	// 		{subscribed ? (
+	// 			<>
+	// 				<Title order={3}>You already have premium, thanks for your support!</Title>
+	// 				<Text>You can gift premium to other people below</Text>
+	// 				<Text>
+	// 					Your premium will expire on <strong>{new Date(expiresAt)?.toUTCString()}</strong>
+	// 				</Text>
+	// 				<Divider variant="solid" my="md" />
+	// 			</>
+	// 		) : null}
+	// 		<Collapse in={!checkout}>
+	// 			<Text>
+	// 				Hosting and maintaining this bot is unfortunately not free. You can support the developer by purchasing a <strong>non-recurring</strong> payment.
+	// 			</Text>
+	// 			<Text>
+	// 				If you have any questions, please email <strong>contact@ourabot.com</strong>. All payments are non-refundable.
+	// 			</Text>
+	// 			<Box>
+	// 				<Paper
+	// 					m="sm"
+	// 					style={{
+	// 						backgroundColor: 'transparent',
+	// 					}}
+	// 				>
+	// 					{/*
+	// 					<Card.Section>
+	// 						<Text mb="xs" weight={500}>
+	// 							Premium Features:
+	// 						</Text>
+	// 						<Grid grow>
+	// 							<Grid.Col md={6} lg={3}>
+	// 								<Card>
+	// 									<Title order={4}>Feature Title</Title>
+	// 									<Text>Feature description will go here, describing what the feature does.</Text>
+	// 								</Card>
+	// 							</Grid.Col>
+	// 							<Grid.Col md={6} lg={3}>
+	// 								<Card>
+	// 									<Title order={4}>Feature Title</Title>
+	// 									<Text>Feature description will go here, describing what the feature does.</Text>
+	// 								</Card>
+	// 							</Grid.Col>
+	// 							<Grid.Col md={6} lg={3}>
+	// 								<Card>
+	// 									<Title order={4}>Feature Title</Title>
+	// 									<Text>Feature description will go here, describing what the feature does.</Text>
+	// 								</Card>
+	// 							</Grid.Col>
+	// 							<Grid.Col md={6} lg={3}>
+	// 								<Card>
+	// 									<Title order={4}>Feature Title</Title>
+	// 									<Text>Feature description will go here, describing what the feature does.</Text>
+	// 								</Card>
+	// 							</Grid.Col>
+	// 						</Grid>
+	// 					</Card.Section>
+	// 						*/}
+	// 				</Paper>
+	// 			</Box>
+
+	// 			<Divider variant="solid" my="md" />
+	// 			<Slider
+	// 				my="lg"
+	// 				value={months}
+	// 				onChange={setMonths}
+	// 				min={1}
+	// 				max={12}
+	// 				label={(value) => `${value} month${value == 1 ? '' : 's'}`}
+	// 				mx="md"
+	// 				marks={[
+	// 					{ value: 1 },
+	// 					{ value: 2 },
+	// 					{ value: 3 },
+	// 					{ value: 4 },
+	// 					{ value: 5 },
+	// 					{ value: 6 },
+	// 					{ value: 7 },
+	// 					{ value: 8 },
+	// 					{ value: 9 },
+	// 					{ value: 10 },
+	// 					{ value: 11 },
+	// 					{ value: 12 },
+	// 				]}
+	// 			/>
+	// 			<Title order={2}>
+	// 				{`$${pricePoints[months]} USD`}{' '}
+	// 				{months > 4 && (
+	// 					<>
+	// 						<Badge size="md" variant="filled">
+	// 							Save {Math.round((100 * (4 * months - pricePoints[months])) / (4 * months))}%
+	// 						</Badge>
+	// 					</>
+	// 				)}
+	// 			</Title>
+	// 			<Text size="lg" mb="md">
+	// 				{months} month{months == 1 ? '' : 's'}
+	// 			</Text>
+
+	// 			<Checkbox
+	// 				label="Gift to another user"
+	// 				checked={gifting}
+	// 				onChange={(event) => {
+	// 					if (subscribed && !event.target.checked) return;
+	// 					setGifting(event.target.checked);
+	// 					setRecepient('');
+	// 				}}
+	// 			/>
+	// 			<Collapse in={gifting}>
+	// 				<TextInput
+	// 					label="Gift to"
+	// 					error={error}
+	// 					autoFocus
+	// 					placeholder="Username"
+	// 					value={recepient}
+	// 					onChange={(event) => {
+	// 						setRecepient(event.target.value);
+	// 						setError('');
+	// 					}}
+	// 				/>
+	// 			</Collapse>
+	// 			<Button
+	// 				my="md"
+	// 				fullWidth
+	// 				onClick={() => {
+	// 					handleCheckout();
+	// 				}}
+	// 			>
+	// 				Checkout
+	// 			</Button>
+	// 		</Collapse>
+	// 		{checkout && (
+	// 			<>
+	// 				<UnstyledButton
+	// 					onClick={() => {
+	// 						setCheckout(false);
+	// 					}}
+	// 				>
+	// 					<ArrowBackUp />
+	// 				</UnstyledButton>
+	// 				<Center>
+	// 					<Stack>
+	// 						<Text size="lg" align="center">
+	// 							<strong>
+	// 								${pricePoints[realMonths]} · {realMonths} month{realMonths == 1 ? '' : 's'}
+	// 							</strong>
+	// 						</Text>
+	// 						{console.log(channel)}
+	// 						<Text align="center">
+	// 							<Text
+	// 								variant="link"
+	// 								component="a"
+	// 								href={`https://twitch.tv/${gifting && recepient.length > 0 ? recepient : channel.login}`}
+	// 								target="_blank"
+	// 							>
+	// 								https://twitch.tv/{gifting && recepient.length > 0 ? recepient : channel.login}
+	// 							</Text>
+	// 						</Text>
+	// 						<PayPalScriptProvider
+	// 							options={{
+	// 								'client-id': cid,
+	// 								components: 'buttons',
+	// 								currency: 'USD',
+	// 							}}
+	// 						>
+	// 							<ButtonWrapper
+	// 								amount={pricePoints[realMonths]}
+	// 								currency={currency}
+	// 								showSpinner={true}
+	// 								recepient={gifting && recepient.length > 0 ? recepient : channel.login}
+	// 								months={realMonths}
+	// 							/>
+	// 						</PayPalScriptProvider>
+	// 					</Stack>
+	// 				</Center>
+	// 			</>
+	// 		)}
+	// 	</>
+	// );
 }
 
 const currency = 'USD';
