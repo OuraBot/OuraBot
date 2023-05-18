7ca56ba93943b897ed5e9d7416ef2e56c8c780a9
Auro <35087590+MrAuro@users.noreply.github.com>
2023-05-18 16:30:09 -0400

    Subscribe to pubsub in EventManager JOIN

--- Bot/src/Utils/Redis/Events/UPDATE/Join.ts
+++ Bot/src/Utils/Redis/Events/UPDATE/Join.ts
@@ -8,9 +8,28 @@ export default function handler(Event: Event): Promise<Event> {
 	const channel = ob.channels.find((c) => c.login === Event.data.login);
 	if (!channel) {
 		ob.channels.push({
-			id: Event.data.id,
+			id: Event.userId,
 			login: Event.data.login,
 			isMod: false,
+		});
+
+		ob.twitch.pubsubClient.onModAction(ob.config.twitch_id, channel.id, (data) => {
+			ob.logger.debug(`Received mod action for ${channel.login}: ${data.type}`, 'ob.eventmanager.update.join');
+
+			switch (data.type) {
+				case 'moderator_added':
+					{
+						ob.channels.find((c) => c.id === channel.id).isMod = true;
+						ob.twitch.say(channel.login, `I am now moderator; all commands are now available!`);
+					}
+					break;
+				case 'moderator_removed':
+					{
+						ob.channels.find((c) => c.id === channel.id).isMod = false;
+						ob.twitch.say(channel.login, `I am no longer moderator; bot functionality is now limited.`);
+					}
+					break;
+			}
 		});
 	}
 
