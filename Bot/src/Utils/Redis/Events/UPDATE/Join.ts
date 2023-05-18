391b07989f2199c15740664103c39e8903b6da00
Auro <35087590+MrAuro@users.noreply.github.com>
2023-05-18 16:40:50 -0400

    Use appropriate IDs for UPDATE/Join

--- Bot/src/Utils/Redis/Events/UPDATE/Join.ts
+++ Bot/src/Utils/Redis/Events/UPDATE/Join.ts
@@ -13,20 +13,20 @@ export default function handler(Event: Event): Promise<Event> {
 			isMod: false,
 		});
 
-		ob.twitch.pubsubClient.onModAction(ob.config.twitch_id, channel.id, (data) => {
-			ob.logger.debug(`Received mod action for ${channel.login}: ${data.type}`, 'ob.eventmanager.update.join');
+		ob.twitch.pubsubClient.onModAction(ob.config.twitch_id, Event.userId, (data) => {
+			ob.logger.debug(`Received mod action for ${Event.data.login}: ${data.type}`, 'ob.eventmanager.update.join');
 
 			switch (data.type) {
 				case 'moderator_added':
 					{
-						ob.channels.find((c) => c.id === channel.id).isMod = true;
-						ob.twitch.say(channel.login, `I am now moderator; all commands are now available!`);
+						ob.channels.find((c) => c.id === Event.userId).isMod = true;
+						ob.twitch.say(Event.data.login, `I am now moderator; all commands are now available!`);
 					}
 					break;
 				case 'moderator_removed':
 					{
-						ob.channels.find((c) => c.id === channel.id).isMod = false;
-						ob.twitch.say(channel.login, `I am no longer moderator; bot functionality is now limited.`);
+						ob.channels.find((c) => c.id === Event.userId).isMod = false;
+						ob.twitch.say(Event.data.login, `I am no longer moderator; bot functionality is now limited.`);
 					}
 					break;
 			}
