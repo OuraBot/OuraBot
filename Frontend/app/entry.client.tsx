4b5c042369fecb38faabef85efd49fc791539994
Auro <35087590+MrAuro@users.noreply.github.com>
2023-05-19 12:49:39 -0400

    Add sentry monitoring to Frontend

--- Frontend/app/entry.client.tsx
+++ Frontend/app/entry.client.tsx
@@ -1,6 +1,24 @@
 import { RemixBrowser } from '@remix-run/react';
 import { hydrate } from 'react-dom';
 import { ClientProvider } from '@mantine/remix';
+import { useLocation, useMatches } from '@remix-run/react';
+import * as Sentry from '@sentry/remix';
+import { useEffect } from 'react';
+
+Sentry.init({
+	dsn: 'https://5c0abffe843d4fab8a7915be315b3058:5af23cd2c9b044b1852cdec606f4c999@o4505139595575296.ingest.sentry.io/4505211544076288',
+	integrations: [
+		new Sentry.BrowserTracing({
+			routingInstrumentation: Sentry.remixRouterInstrumentation(useEffect, useLocation, useMatches),
+		}),
+		new Sentry.Replay(),
+	],
+	// Performance Monitoring
+	tracesSampleRate: 1.0, // Capture 100% of the transactions, reduce in production!
+	// Session Replay
+	replaysSessionSampleRate: 0.1, // This sets the sample rate at 10%. You may want to change it to 100% while in development and then sample at a lower rate in production.
+	replaysOnErrorSampleRate: 1.0, // If you're not already sampling the entire session, change the sample rate to 100% when sampling sessions where errors occur.
+});
 
 hydrate(
 	<ClientProvider>
--- Frontend/app/entry.server.tsx
+++ Frontend/app/entry.server.tsx
@@ -2,15 +2,20 @@ import { renderToString } from 'react-dom/server';
 import { RemixServer } from '@remix-run/react';
 import type { EntryContext } from '@remix-run/node';
 import { injectStyles, createStylesServer } from '@mantine/remix';
+import { useLocation, useMatches } from '@remix-run/react';
+import * as Sentry from '@sentry/remix';
+import { useEffect } from 'react';
+
+Sentry.init({
+	dsn: 'https://5c0abffe843d4fab8a7915be315b3058:5af23cd2c9b044b1852cdec606f4c999@o4505139595575296.ingest.sentry.io/4505211544076288',
+	integrations: [],
+	// Performance Monitoring
+	tracesSampleRate: 1.0, // Capture 100% of the transactions, reduce in production!
+});
 
 const server = createStylesServer();
 
-export default function handleRequest(
-	request: Request,
-	responseStatusCode: number,
-	responseHeaders: Headers,
-	remixContext: EntryContext
-) {
+export default function handleRequest(request: Request, responseStatusCode: number, responseHeaders: Headers, remixContext: EntryContext) {
 	let markup = renderToString(<RemixServer context={remixContext} url={request.url} />);
 	responseHeaders.set('Content-Type', 'text/html');
 
