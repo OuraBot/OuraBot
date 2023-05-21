e81d151bdf9252c3c5ed07c4b562ec0553e7aec8
Auro <35087590+MrAuro@users.noreply.github.com>
2023-05-21 11:57:48 -0400

    Add potential fix for uncentered modals

--- Frontend/app/entry.client.tsx
+++ Frontend/app/entry.client.tsx
@@ -4,6 +4,7 @@ import { ClientProvider } from '@mantine/remix';
 import { useLocation, useMatches } from '@remix-run/react';
 import * as Sentry from '@sentry/remix';
 import { useEffect } from 'react';
+import { createEmotionCache } from '@mantine/core';
 
 Sentry.init({
 	dsn: 'https://5c0abffe843d4fab8a7915be315b3058:5af23cd2c9b044b1852cdec606f4c999@o4505139595575296.ingest.sentry.io/4505211544076288',
@@ -20,8 +21,10 @@ Sentry.init({
 	replaysOnErrorSampleRate: 1.0, // If you're not already sampling the entire session, change the sample rate to 100% when sampling sessions where errors occur.
 });
 
+let emotionCache = createEmotionCache({ key: 'mantine' });
+
 hydrate(
-	<ClientProvider>
+	<ClientProvider emotionCache={emotionCache}>
 		<RemixBrowser />
 	</ClientProvider>,
 	document
