2f53840eb0c943b1333abb5ae04617ab87869dae
Auro <35087590+MrAuro@users.noreply.github.com>
2023-05-23 10:05:21 -0700

    Update commands page to show real values (#32)

--- Frontend/app/routes/commands.$streamerLogin.tsx
+++ Frontend/app/routes/commands.$streamerLogin.tsx
@@ -1,122 +1,152 @@
-import { Avatar, Button, Center, Container, Group, Table, Text } from '@mantine/core';
+import { Avatar, Button, Center, Container, Divider, Group, Table, Text, Title } from '@mantine/core';
 import type { LoaderArgs } from '@remix-run/node';
 import { fetch, json } from '@remix-run/node';
 import { Link, useLoaderData } from '@remix-run/react';
 import { ChannelModel } from '~/services/models/Channel';
+import { query, redisConnect } from '~/services/redis.server';
 
-export type IvrFiUser = {
-	status: number;
-	banned: boolean;
-	displayName: string;
+type PublicChannel = {
 	login: string;
+	profile_image_url: string;
 	id: string;
-	bio: string;
-	chatColor: string;
-	logo: string;
-	partner: boolean;
-	affiliate: boolean;
-	bot: boolean;
-	createdAt: string;
-	updatedAt: string;
-	chatSettings: {
-		chatDelayMs: number | null;
-		followersOnlyDurationMinutes: number | null;
-		slowModeDurationSeconds: number | null;
-		blockLinks: boolean | null;
-		isSubscribersOnlyModeEnabled: boolean | null;
-		isEmoteOnlyModeEnabled: boolean | null;
-		isFastSubsModeEnabled: boolean | null;
-		isUniqueChatModeEnabled: boolean | null;
-		requireVerifiedAccount: boolean | null;
-		rules: string[] | null;
-	};
-	badge: string[];
-	roles: {
-		isAffiliate: boolean;
-		isPartner: boolean;
-		isSiteAdmin: boolean | null;
-		isStaff: boolean | null;
-	};
-	settings: {
-		preferredLanguageTag: string;
-	};
-	panels: {
-		id: string;
-	}[];
 };
 
-type UserType = {
-	user: {
-		login: string;
-		id: string;
-		role: number;
-		token: string;
-		managers: string[];
-		banned: string | null;
-		readTos: boolean;
-	} | null;
-
-	ivrUserData: IvrFiUser;
-};
+enum CategoryEnum {
+	Utility = 'Utility',
+	Fun = 'Fun',
+	Moderation = 'Moderation',
+}
 
 export async function loader({ params }: LoaderArgs) {
-	// The `user` variable is used for getting the user's data from the Oura Bot database
-	// - to get commands and such.
+	// Since this is a public facing endpoint that makes requests, we need to heavily cache the results.
+	//
 
-	// The `ivrUserData` variable is only for getting the user's profile picture
-	// - or fallback data for if the user doesn't exist in the OB database.
-	let [user, ivrUserData] = await Promise.all([
-		ChannelModel.findOne({ login: params.streamerLogin }),
-		(await fetch(`https://api.ivr.fi/twitch/resolve/${params.streamerLogin}`)).json(),
-	]);
+	let streamerLogin = params.streamerLogin?.toLowerCase()?.replace(/^(@|#)|,?$/, '');
 
-	if (!user) {
-		return { user: null, ivrUserData };
+	if (!streamerLogin) {
+	} else {
+		const { pub, sub } = await redisConnect();
+		const cachedUser = await pub.get(`obfrontend:streamer:${streamerLogin}`);
+		if (cachedUser) {
+			console.log('Returning cached user data');
+			return json(JSON.parse(cachedUser));
+		} else {
+			const channel = await ChannelModel.findOne({ login: streamerLogin });
+			if (channel) {
+				console.log('Channel found, returning data: ' + channel.login);
+				const data: PublicChannel = {
+					login: channel.login,
+					profile_image_url: channel.profile_image_url,
+					id: channel.id,
+				};
+
+				const commands = await query('QUERY', 'Commands', channel.token, channel.id);
+
+				if (commands.status !== 200) throw new Error(`QUERY Commands returned error code ${commands.status}`);
+
+				const returnData = {
+					channel: data,
+					commands: commands.data,
+				};
+
+				await pub.set(`obfrontend:commands:${streamerLogin}`, JSON.stringify(returnData), 'EX', 60 * 60 * 12); // 12 hours
+
+				return json(returnData);
+			} else {
+				console.log('Channel not found');
+				return json({ error: 'Channel not found' }, { status: 404 });
+			}
+		}
 	}
-
-	return json({
-		user,
-		ivrUserData,
-	}) as unknown as UserType;
 }
 
 export default function StreamerPage() {
-	const { user, ivrUserData } = useLoaderData<typeof loader>();
+	const { channel, commands } = useLoaderData();
+
+	console.log(channel, commands);
 
 	return (
 		<Container mb="xl">
-			{user ? (
+			{channel ? (
 				<>
 					<Group mt="xl" position="center" mb="sm">
-						<Avatar src={ivrUserData.logo} alt={user.login + "'s profile image."} size="lg" radius="xl" />
-						<h1>{user.login}'s Commands</h1>
+						<Avatar src={channel.profile_image_url} alt={channel.login + "'s profile image."} size="lg" radius="xl" />
+						<h1>{channel.login}'s Commands</h1>
 					</Group>
 					<Text mb="xl" align="center">
-						Below are commands available in {user.login}'s chat.
+						Below are commands available in {channel.login}'s chat.
 					</Text>
 
+					<Title order={2}>Fun Commands</Title>
 					<Table>
 						<thead>
 							<tr>
 								<th>Command</th>
 								<th>Description</th>
 								<th>Usage</th>
+								<th>Permission</th>
 								<th>Enabled</th>
 							</tr>
 						</thead>
 						<tbody>
-							{/* replace this stuff with commands whenever we get that in the database */}
-							{ivrUserData.panels.map((panel) => (
-								//   PLACEHOLDER
-								<tr key={panel.id}>
-									{/* PLACEHOLDER */}
-									<td>{panel.id}</td>
-									<td>{panel.id}</td>
-									<td>{panel.id}</td>
-									<td>{panel.id}</td>
-									{/* PLACEHOLDER */}
+							{commands.defaultCommands['Fun'].map((command: any) => (
+								<tr key={command.name}>
+									<td>{command.name}</td>
+									<td>{command.description}</td>
+									<td>{command.usage}</td>
+									<td>{command.permissions.join(', ')}</td>
+									<td>{command.enabled ? '✔' : '❌'}</td>
 								</tr>
-								// PLACEHOLDER
+							))}
+						</tbody>
+					</Table>
+					<Divider my="md" />
+
+					<Title order={2}>Utility Commands</Title>
+					<Table>
+						<thead>
+							<tr>
+								<th>Command</th>
+								<th>Description</th>
+								<th>Usage</th>
+								<th>Permission</th>
+								<th>Enabled</th>
+							</tr>
+						</thead>
+						<tbody>
+							{commands.defaultCommands['Utility'].map((command: any) => (
+								<tr key={command.name}>
+									<td>{command.name}</td>
+									<td>{command.description}</td>
+									<td>{command.permissions.join(', ')}</td>
+									<td>{command.usage}</td> <td>{command.enabled ? '✔' : '❌'}</td>
+								</tr>
+							))}
+						</tbody>
+					</Table>
+
+					<Divider my="md" />
+
+					<Title order={2}>Moderation Commands</Title>
+					<Table>
+						<thead>
+							<tr>
+								<th>Command</th>
+								<th>Description</th>
+								<th>Usage</th>
+								<th>Permission</th>
+								<th>Enabled</th>
+							</tr>
+						</thead>
+						<tbody>
+							{commands.defaultCommands['Moderation'].map((command: any) => (
+								<tr key={command.name}>
+									<td>{command.name}</td>
+									<td>{command.description}</td>
+									<td>{command.usage}</td>
+									<td>{command.permissions.join(', ')}</td>
+									<td>{command.enabled ? '✔' : '❌'}</td>
+								</tr>
 							))}
 						</tbody>
 					</Table>
@@ -132,8 +162,7 @@ export default function StreamerPage() {
 					}}
 				>
 					<Group>
-						<Avatar src={ivrUserData.logo} alt={ivrUserData.login + "'s profile image."} size="lg" radius="xl" />
-						<h1>{ivrUserData.login}'s data could not be found</h1>
+						<h1>Channel data could not be found</h1>
 					</Group>
 
 					<Center>
