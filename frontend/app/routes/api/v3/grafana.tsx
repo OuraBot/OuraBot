6ac88728c099d2f1724955ed0e8666e87af9e2fa
Auro <35087590+MrAuro@users.noreply.github.com>
2023-05-25 15:46:59 -0400

    Move grafana route

---
+++ frontend/app/routes/api/v3/grafana.tsx
@@ -0,0 +1,10 @@
+import { LoaderFunction, json } from '@remix-run/server-runtime';
+
+export const loader: LoaderFunction = async ({ params, request }) => {
+	const auth = request.headers.get('Authorization');
+	if (!auth) throw new Response('missing auth', { status: 401 });
+
+	if (auth !== '9lr2j36kQ+oSGmY0tEewOl+B7XNWA35lY0v+fXyTvr4=') throw new Response('forbidden', { status: 403 });
+
+	return json({});
+};
