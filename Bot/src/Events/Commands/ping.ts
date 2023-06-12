b4ef3fae79c6ea26addd87361e4b053c8a7e5c41
Auro <35087590+MrAuro@users.noreply.github.com>
2023-06-12 18:17:42 -0400

    Improve ping command output

--- Bot/src/Events/Commands/ping.ts
+++ Bot/src/Events/Commands/ping.ts
@@ -12,9 +12,11 @@ export const cmd = new (class command implements Command {
 	execute = async (ob: OuraBot, user: string, Channel: Channel, args: string[], _message: string, msg: TwitchPrivateMessage, alias: string): Promise<CommandReturn> => {
 		return {
 			success: true,
-			message: `Pong! Serving ${ob.channels.length} channels for ${ob.utils.humanizeTime(process.uptime() * 1000)}. ${ob.utils.stopNanoStopwatch(
-				`interal_message_delay_${msg.id}`
-			)}ms internal delay. Prefix: ${Channel.prefix} (https://status.mrauro.dev/)`,
+			message: `Pong! Serving ${ob.channels.length} channels for ${ob.utils.humanizeTime(process.uptime() * 1000)}. ${Math.round(
+				ob.utils.stopNanoStopwatch(`interal_message_delay_${msg.id}`)
+			)}ms internal delay. Check the status of OuraBot here: https://status.mrauro.dev/ - Commands: https://ourabot.com/commands/${Channel.channel} - Prefix: ${
+				Channel.prefix
+			} `,
 		};
 	};
 })();
