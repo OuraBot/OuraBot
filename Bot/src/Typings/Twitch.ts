0735d65fd1b55d1032ac176d56714590a8074b6f
Auro <35087590+MrAuro@users.noreply.github.com>
2023-06-12 17:39:02 -0400

    Add logging for permissions

--- Bot/src/Typings/Twitch.ts
+++ Bot/src/Typings/Twitch.ts
@@ -197,6 +197,11 @@ export function hasPermission(requiredPermissions: Permission[], user: string, c
 	if (msg.userInfo.isVip) permissions.push(Permission.VIP);
 	if (msg.userInfo.isSubscriber) permissions.push(Permission.Subscriber);
 
+	ob.logger.debug(
+		`User: ${user} | Channel: ${channel} | Permissions: ${permissions.join(', ')} | Required Permissions: ${requiredPermissions.join(', ')}`,
+		'ob.twitch.permissions'
+	);
+
 	if (requiredPermissions.length === 0) return true;
 
 	return requiredPermissions.every((permission) => permissions.includes(permission));
