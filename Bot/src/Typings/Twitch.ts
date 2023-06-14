77c7f10ddda4e4d19de8e17fde17fddc1289d0d4
Auro <35087590+MrAuro@users.noreply.github.com>
2023-06-13 20:14:23 -0400

    Remove unused code related to the options property in the Module interface

--- Bot/src/Typings/Twitch.ts
+++ Bot/src/Typings/Twitch.ts
@@ -173,13 +173,6 @@ export interface Module {
 	description: string;
 	requiresMod?: boolean;
 	hidden?: boolean;
-	options?: {
-		[key: string]: {
-			type: 'boolean' | 'number' | 'string';
-			min?: number;
-			max?: number;
-		};
-	};
 	execute: (ob: OuraBot, user: string, channel: Channel, message: string, msg: TwitchPrivateMessage, data?: any) => void;
 }
 
