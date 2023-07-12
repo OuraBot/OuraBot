6f0789ddd19a7dae59691a9e8baca429bac858e7
Auro <35087590+MrAuro@users.noreply.github.com>
2023-07-12 16:22:33 -0400

    Remove unneccesary console logs

--- Frontend/app/routes/dashboard/premium.success.tsx
+++ Frontend/app/routes/dashboard/premium.success.tsx
@@ -59,7 +59,7 @@ export const loader: LoaderFunction = async ({ request }: LoaderArgs) => {
 export default function PremiumSuccess() {
 	const data = useLoaderData();
 	const particlesInit = useCallback(async (engine: Engine) => {
-		console.log(engine);
+		// console.log(engine);
 
 		// you can initialize the tsParticles instance (engine) here, adding custom shapes or presets
 		// this loads the tsparticles package bundle, it's the easiest method for getting everything ready
@@ -68,7 +68,7 @@ export default function PremiumSuccess() {
 	}, []);
 
 	const particlesLoaded = useCallback(async (container: Container | undefined) => {
-		await console.log(container);
+		// await console.log(container);
 	}, []);
 	return (
 		<>
