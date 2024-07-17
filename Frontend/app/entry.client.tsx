e6598df0b446b82f58b0bbc47a194adf703b2e0e
Auro <35087590+MrAuro@users.noreply.github.com>
2024-07-16 22:09:41 -0400

    dev: Reduce sentry.io usage

--- Frontend/app/entry.client.tsx
+++ Frontend/app/entry.client.tsx
@@ -7,7 +7,7 @@ import { useEffect } from 'react';
 import { createEmotionCache } from '@mantine/core';
 
 Sentry.init({
-	denyUrls: ['https://ourabot.com/api/grafana/admin'],
+	denyUrls: ['https://ourabot.com/api/grafana/admin', 'https://ourabot.com'],
 	beforeSendTransaction(event) {
 		if (event.transaction?.includes('/api/grafana')) {
 			return null;
@@ -26,9 +26,9 @@ Sentry.init({
 		new Sentry.Replay(),
 	],
 	// Performance Monitoring
-	tracesSampleRate: 0.2, // Capture 100% of the transactions, reduce in production!
+	tracesSampleRate: 0, // Capture 100% of the transactions, reduce in production!
 	// Session Replay
-	replaysSessionSampleRate: 0.1, // This sets the sample rate at 10%. You may want to change it to 100% while in development and then sample at a lower rate in production.
+	replaysSessionSampleRate: 0, // This sets the sample rate at 10%. You may want to change it to 100% while in development and then sample at a lower rate in production.
 	replaysOnErrorSampleRate: 1.0, // If you're not already sampling the entire session, change the sample rate to 100% when sampling sessions where errors occur.
 });
 
